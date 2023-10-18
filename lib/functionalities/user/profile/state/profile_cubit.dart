f63a4f39b93de72977e03f9767fe5cf753897117
Camillo Bucciarelli <af35609@enel.com>
2023-10-18 15:26:11 +0200

    feat: profile view sign out button

--- lib/functionalities/user/profile/state/profile_cubit.dart
+++ lib/functionalities/user/profile/state/profile_cubit.dart
@@ -4,10 +4,21 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:injectable/injectable.dart';
 
 import '../../domain/use_cases/get_signed_user.dart';
+import '../../domain/use_cases/sign_out_use_case.dart';
 
 part 'profile_state.dart';
 
 @injectable
 class ProfileCubit extends Cubit<ProfileState> {
-  ProfileCubit(GetSignedUserUseCase getSignedUserUseCase) : super(ProfileState(getSignedUserUseCase()));
+  ProfileCubit(
+    GetSignedUserUseCase getSignedUserUseCase,
+    this._signOutUseCase,
+  ) : super(ProfileState(getSignedUserUseCase()));
+
+  final SignOutUseCase _signOutUseCase;
+
+  void signOut() {
+    _signOutUseCase();
+    emit(const ProfileState(null));
+  }
 }
