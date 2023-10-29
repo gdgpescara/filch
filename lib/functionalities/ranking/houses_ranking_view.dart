45eec69672e208c66323ffbb2134db3a1383ba3a
Camillo Bucciarelli <af35609@enel.com>
2023-10-29 11:51:48 +0100

    feat: added houses ranking page

--- lib/functionalities/ranking/houses_ranking_view.dart
+++ lib/functionalities/ranking/houses_ranking_view.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../dependency_injection/dependency_injection.dart';
+import '../../i18n/strings.g.dart';
+import '../_shared/widgets/loader_animation.dart';
+import 'state/houses_ranking_cubit.dart';
+import 'widgets/houses_ranking_list.dart';
 
 class HousesRankingView extends StatelessWidget {
-  const HousesRankingView({Key? key}) : super(key: key);
+  const HousesRankingView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: BlocProvider<HousesRankingCubit>(
+        create: (_) => injector()..loadHouses(),
+        child: BlocBuilder<HousesRankingCubit, HousesRankingState>(
+          builder: (context, state) {
+            return switch (state) {
+              HousesRankingLoading() => const Center(child: LoaderAnimation()),
+              HousesRankingLoaded() => const HousesRankingList(),
+              HousesRankingFailure() => Center(child: Text(t.commons.errors.generic_retry)),
+            };
+          },
+        ),
+      ),
+    );
   }
 }
---
+++ lib/functionalities/ranking/state/houses_ranking_cubit.dart
@@ -0,0 +1,34 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../_shared/error_handling/future_extension.dart';
+import '../../_shared/error_handling/stream_extension.dart';
+import '../../_shared/models/house.dart';
+import '../../user/use_cases/get_signed_user_house_use_case.dart';
+import '../use_cases/get_houses_use_case.dart';
+
+part 'houses_ranking_state.dart';
+
+@injectable
+class HousesRankingCubit extends Cubit<HousesRankingState> {
+  HousesRankingCubit(this._getHousesUseCase, this._getSignedUserHouseUseCase) : super(const HousesRankingLoading());
+
+  final GetHousesUseCase _getHousesUseCase;
+  final GetSignedUserHouseUseCase _getSignedUserHouseUseCase;
+
+  void loadHouses() {
+    if (state.userHouse == null) {
+      _getSignedUserHouseUseCase().actions(
+        progress: () => emit(const HousesRankingLoading()),
+        success: (userHouse) => emit(HousesRankingLoading(userHouse)),
+        failure: (_) => emit(const HousesRankingFailure(null)),
+      );
+    }
+    _getHousesUseCase().actions(
+      progress: () => emit(HousesRankingLoading(state.userHouse)),
+      success: (houses) => emit(HousesRankingLoaded(state.userHouse, houses: houses)),
+      failure: (_) => emit(HousesRankingFailure(state.userHouse)),
+    );
+  }
+}
