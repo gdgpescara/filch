af1d2d04c28cb3b03be171317f5ef5fefed8c540
Camillo Bucciarelli <af35609@enel.com>
2023-10-29 14:25:43 +0100

    feat: added user points

---
+++ lib/functionalities/user/user_points/user_points_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserPointsWidget extends StatelessWidget {
+const UserPointsWidget({Key? key}) : super(key: key);
+
+@override
+Widget build(BuildContext context) {
+return Container();
+}
+}
