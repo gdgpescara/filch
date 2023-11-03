a77522bd9ab57836cbc0ff794203a445a03b3700
Camillo Bucciarelli <af35609@enel.com>
2023-11-03 16:13:55 +0100

    fix: quest timer

--- functions/src/features/points/assign-points.ts
+++ functions/src/features/points/assign-points.ts
@@ -3,12 +3,16 @@ import {getSignedInUser} from "../../shared/get_signed_in_user";
 import {getFirestore, Timestamp} from "firebase-admin/firestore";
 import {Points} from "./types/points";
 import {getAuth} from "firebase-admin/auth";
+import {PointsTypeEnum} from "./types/points-type-enum";
+import {ActiveQuest} from "../quests/types/active-quest";
 
 export const assignPoints = onCall(
   {region: "europe-west3"},
   async (request) => {
     const loggedUser = await getSignedInUser(request);
     const assignedPoints = request.data.points;
+    const type = request.data.type;
+    const questId = request.data.quest;
     const users = await Promise.all(
       request.data.users
         .map(async (uid: string) => await getAuth().getUser(uid)),
@@ -25,7 +29,7 @@ export const assignPoints = onCall(
       }
     }
     const points = <Points>{
-      type: request.data.type,
+      type: type,
       points: assignedPoints * multiplier,
       assignedBy: loggedUser,
       assignedAt: Timestamp.now(),
@@ -51,8 +55,29 @@ export const assignPoints = onCall(
           .doc(),
         points,
       );
+      if (type == PointsTypeEnum.quest) {
+        const docRef = await getFirestore()
+          .collection("users")
+          .doc(user.uid)
+          .get();
+        const activeQuest = docRef.data()?.activeQuest as ActiveQuest;
+        if (activeQuest.quest.id === questId) {
+          batch.update(
+            getFirestore().collection("users").doc(user.uid),
+            {activeQuest: null}
+          );
+        }
+        batch.delete(
+          getFirestore()
+            .collection("quests")
+            .doc(questId)
+            .collection("queue")
+            .doc(user.uid)
+        );
+      }
     }
     await batch.commit();
 
     return true;
-  });
+  }
+);
