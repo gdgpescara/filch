abdca2dc048ef5be8df5d9ffe90df4c891561de9
Camillo Bucciarelli <af35609@enel.com>
2023-11-03 10:47:36 +0100

    feat: added members update on user removal

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,17 +4,20 @@ import {searchForQuest} from "./features/quests/search-for-quest";
 import {updateHouseMembers} from "./features/sorting-ceremony/house-manager";
 import {actorQueueSentinel} from "./features/quests/actor-quest-queue-manager";
 import {submitAnswer} from "./features/quests/submit-answer";
-// import {
-//   expiredActiveQuestCleaner,
-//   questQueueCleaner,
-// } from "./features/quests/quest-cron";
+import {
+  expiredActiveQuestCleaner,
+  questQueueCleaner,
+} from "./features/quests/quest-cron";
 import {
   memberPointsSentinel,
   userPointsSentinel,
 } from "./features/points/points-manager";
 import {assignPoints} from "./features/points/assign-points";
+import {onUserDeleteSentinel} from "./features/user/user_manager";
 
 initializeApp();
+
+exports.onUserDeleteSentinel = onUserDeleteSentinel;
 
 exports.sortingCeremony = sortingCeremony;
 exports.updateHouseMembers = updateHouseMembers;
@@ -24,8 +27,8 @@ exports.actorQueueSentinel = actorQueueSentinel;
 exports.submitAnswer = submitAnswer;
 
 // Cron jobs [Start]
-// exports.expiredActiveQuestCleaner = expiredActiveQuestCleaner;
-// exports.questQueueCleaner = questQueueCleaner;
+exports.expiredActiveQuestCleaner = expiredActiveQuestCleaner;
+exports.questQueueCleaner = questQueueCleaner;
 // Cron jobs [End]
 
 exports.assignPoints = assignPoints;
