13f6c8bdaee2be765b2390f4edcd94c47fb9b586
Camillo bucciarelli <bucciarelli.camillo92@gmail.com>
2024-11-07 16:10:19 +0100

    fix: ranking closing view

---
+++ lib/app/thanks/thanks_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:i18n/i18n.dart';
+import 'package:ui/ui.dart';
+
+class ThanksPage extends StatelessWidget {
+  const ThanksPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(t.common.thanks, style: context.getTextTheme(TextThemeType.themeSpecific).bodyMedium),
+      ),
+    );
+  }
+}
