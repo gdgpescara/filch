f43628cab61db44258da54e127be08dcaa6f3958
Camillo Bucciarelli <bucciarelli.camillo92@gmail.comn>
2025-09-01 15:13:12 +0200

    chore: update dependencies and devDependencies in node-functions
    - Bump @google-cloud/vision from 5.3.2 to 5.3.3
    - Bump firebase-admin from 13.4.0 to 13.5.0
    - Update devDependencies:
      - @eslint/js to 9.34.0
      - @typescript-eslint/eslint-plugin and parser to 8.41.0
      - eslint to 9.34.0
      - typescript to 5.9.2
    
    refactor: clean up unused exports in index.ts
    
    - Uncommented user management exports
    - Commented out unused exports for points and community features
    
    style: remove unnecessary comment in t-shirt notification schedule
    
    - Removed eslint-disable comment for max-len
    
    chore: update tsconfig.dev.json for better linting options
    
    - Added noUnusedLocals and noUnusedParameters options
    
    feat: add new dependencies in schedule package
    
    - Added core, cloud_firestore, equatable, injectable, json_annotation, and collection dependencies
    - Updated dev_dependencies with build_runner, injectable_generator, json_serializable, mockito, and fake_cloud_firestore

--- functions/main.py
+++ functions/main.py
@@ -1,25 +1,15 @@
 from firebase_admin import initialize_app
 
-'''
-from features.user.user_manager import on_user_create, on_user_delete
+initialize_app()
+
+# from features.user.user_manager import on_user_create, on_user_delete
 from features.user.t_shirt_pickup import t_shirt_pickup
 from features.user.t_shirt_notification import t_shirt_notification_schedule
-from features.points.points_manager import user_points_sentinel
-'''
 
-initialize_app()
 
 from features.points.scan_other_attendee import scan_other_attendee
 from features.points.points_manager import user_points_sentinel
 from features.points.submit_answer import submit_answer
 from features.points.assign_points import assign_points
-
-'''
-# User management functions
-on_user_create_function = on_user_create
-on_user_delete_function = on_user_delete
-t_shirt_pickup_function = t_shirt_pickup
-t_shirt_notification_schedule_function = t_shirt_notification_schedule
-'''
 
 
--- node-functions/src/index.ts
+++ node-functions/src/index.ts
@@ -1,33 +1,20 @@
 import {initializeApp} from "firebase-admin/app";
 import {searchForQuest} from "./features/quests/search-for-quest";
 import {actorQueueSentinel} from "./features/quests/actor-quest-queue-manager";
-import {userPointsSentinel} from "./features/points/points-manager";
-import {assignPoints} from "./features/points/assign-points";
-import {
-  onUserCreateSentinel,
-  onUserDeleteSentinel,
-} from "./features/user/user_manager";
 import {removeActiveQuest} from "./features/quests/remove-active-quest";
-import {tShirtPuckUp} from "./features/user/t-shirt-pick-up";
-import {scanOtherAttendee} from "./features/points/scan-other-attendee";
 import {submitAnswer} from "./features/points/submit-answer";
-import {cutPeople} from "./features/communities/cut-people";
-import {detectEmotions} from "./features/communities/detect-emotions";
-import {getFadeFromTime} from "./features/communities/get-fade-from-time";
-import {
-  getFadeFromTimeTimestamp,
-} from "./features/communities/get-fade-from-time-timestamp";
+import { onUserCreateSentinel, onUserDeleteSentinel } from "./features/user/user_manager";
 
 initializeApp();
 
-// exports.onUserDeleteSentinel = onUserDeleteSentinel;
-// exports.onUserCreateSentinel = onUserCreateSentinel;
+exports.onUserDeleteSentinel = onUserDeleteSentinel;
+exports.onUserCreateSentinel = onUserCreateSentinel;
 // exports.tShirtPickUp = tShirtPuckUp;
 
 exports.actorQueueSentinel = actorQueueSentinel;
 exports.searchForQuest = searchForQuest;
 exports.submitAnswer = submitAnswer;
-exports.scanOtherAttendee = scanOtherAttendee;
+// exports.scanOtherAttendee = scanOtherAttendee;
 exports.removeActiveQuest = removeActiveQuest;
 
 // Cron jobs [Start]
@@ -36,13 +23,13 @@ exports.removeActiveQuest = removeActiveQuest;
 // exports.tShirtNotificationSchedule = tShirtNotificationSchedule;
 // Cron jobs [End]
 
-exports.assignPoints = assignPoints;
-exports.userPointsSentinel = userPointsSentinel;
+// exports.assignPoints = assignPoints;
+// exports.userPointsSentinel = userPointsSentinel;
 
-exports.cutPeople = cutPeople;
-exports.detectEmotions = detectEmotions;
+// exports.cutPeople = cutPeople;
+// exports.detectEmotions = detectEmotions;
 // Cron jobs [Start]
 // exports.fadeFromTime = fadeFromTime;
 // Cron jobs [End]
-exports.getFadeFromTime = getFadeFromTime;
-exports.getFadeFromTimeTimestamp = getFadeFromTimeTimestamp;
+// exports.getFadeFromTime = getFadeFromTime;
+// exports.getFadeFromTimeTimestamp = getFadeFromTimeTimestamp;
