343eb87da6a682d8bbe92c8d9f5fe8943bbf1a8a
Camillo Bucciarelli <bucciarelli.camillo92@gmail.comn>
2025-10-09 14:37:14 +0200

    chore: update firebase-functions dependency to version 6.5.0
    refactor: clean up index.ts by removing commented-out exports and adding sendTShirtNotification

--- node-functions/src/index.ts
+++ node-functions/src/index.ts
@@ -2,19 +2,17 @@ import { initializeApp } from "firebase-admin/app";
 import { searchForQuest } from "./features/quests/search-for-quest";
 import { actorQueueSentinel } from "./features/quests/actor-quest-queue-manager";
 import { removeActiveQuest } from "./features/quests/remove-active-quest";
-import { submitAnswer } from "./features/points/submit-answer";
 import { onUserCreateSentinel, onUserDeleteSentinel } from "./features/user/user_manager";
+import { sendTShirtNotification } from "./features/user/t-shirt-notification-schedule";
 
 initializeApp();
 
 exports.onUserDeleteSentinel = onUserDeleteSentinel;
 exports.onUserCreateSentinel = onUserCreateSentinel;
-// exports.tShirtPickUp = tShirtPuckUp;
 
 exports.actorQueueSentinel = actorQueueSentinel;
 exports.searchForQuest = searchForQuest;
-exports.submitAnswer = submitAnswer;
-// exports.scanOtherAttendee = scanOtherAttendee;
+exports.sendTShirtNotification = sendTShirtNotification;
 exports.removeActiveQuest = removeActiveQuest;
 
 // Cron jobs [Start]
@@ -22,14 +20,3 @@ exports.removeActiveQuest = removeActiveQuest;
 // exports.removedQuestSchedule = removedQuestSchedule;
 // exports.tShirtNotificationSchedule = tShirtNotificationSchedule;
 // Cron jobs [End]
-
-// exports.assignPoints = assignPoints;
-// exports.userPointsSentinel = userPointsSentinel;
-
-// exports.cutPeople = cutPeople;
-// exports.detectEmotions = detectEmotions;
-// Cron jobs [Start]
-// exports.fadeFromTime = fadeFromTime;
-// Cron jobs [End]
-// exports.getFadeFromTime = getFadeFromTime;
-// exports.getFadeFromTimeTimestamp = getFadeFromTimeTimestamp;
